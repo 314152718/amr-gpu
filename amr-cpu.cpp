b9ad9c6c7ae367f571caa77eadafe02391a509b5
zandalman <zandalman@gmail.com>
2023-11-30 18:15:48 -0500

    Finished first pass at gradient function

--- amr-cpu.cpp
+++ amr-cpu.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 Cell grid[N_cell_max];
 unordered_map<idx4, Cell> hashtable;
+unordered_map<idx4, Cell>:: iterator hashtable_itr;
 
 uint transposeToHilbert(const uint X[N_dim], const int L) {
     uint hindex = 0, n = 0;
@@ -22,7 +23,7 @@ uint transposeToHilbert(const uint X[N_dim], const int L) {
     return hindex;
 }
 
-void hilbertToTranspose(uint X[N_dim], const uint hindex, const int L) {
+void hilbertToTranspose(const uint hindex, const int L, uint (&X)[N_dim]) {
     uint h = hindex;
     for (short i = 0; i < N_dim; ++i) X[i] = 0;
     for (short i = 0; i < N_dim * L; ++i) {
@@ -40,7 +41,7 @@ idx4: 4-index
 */
 void getHindex(idx4 idx_cell, uint& hindex) { // Axes to transpose
     uint X[3];
-    for (int i = 0; i++; i < N_dim) {
+    for (short i = 0; i++; i < N_dim) {
         X[i] = idx_cell.idx3[i];
     }
     int L = idx_cell.L;
@@ -49,7 +50,7 @@ void getHindex(idx4 idx_cell, uint& hindex) { // Axes to transpose
     // Inverse undo
     for (q = m; q > 1; q >>= 1) {
         p = q - 1;
-        for(i = X[0]; i < N_dim; i++) {
+        for(short i = X[0]; i < N_dim; i++) {
             if (X[i] & q ) { // invert 
                 X[0] ^= p;
             } else { // exchange
@@ -60,7 +61,7 @@ void getHindex(idx4 idx_cell, uint& hindex) { // Axes to transpose
         }
     }
     // Gray encode
-    for (i = 1; i < N_dim; i++) {
+    for (short i = 1; i < N_dim; i++) {
         X[i] ^= X[i-1];
     }
     t = 0;
@@ -69,7 +70,7 @@ void getHindex(idx4 idx_cell, uint& hindex) { // Axes to transpose
             t ^= q - 1;
         }
     }
-    for (i = 0; i < N_dim; i++) {
+    for (short i = 0; i < N_dim; i++) {
         X[i] ^= t;
     }
     // convert transpose to hindex
@@ -92,10 +93,9 @@ void getHindexInv(uint hindex, int L, idx4& idx_cell) { // Transpose to axes
 
     
     uint n = 2 << (L - 1), p, q, t;
-    int i;
     // Gray decode by H ^ (H/2)
     t = X[N_dim - 1] >> 1;
-    for (i = N_dim - 1; i > 0; i--) {
+    for (short i = N_dim - 1; i > 0; i--) {
         X[i] ^= X[i - 1];
     }
     X[0] ^= t;
@@ -103,7 +103,7 @@ void getHindexInv(uint hindex, int L, idx4& idx_cell) { // Transpose to axes
     for (q = 2; q != n; q <<= 1) {
         p = q - 1;
     }
-    for (i = N_dim - 1; i > 0; i--) {
+    for (short i = N_dim - 1; i > 0; i--) {
         if(X[i] & q) { // invert
             X[0] ^= p;
         } else {
@@ -112,7 +112,7 @@ void getHindexInv(uint hindex, int L, idx4& idx_cell) { // Transpose to axes
             X[i] ^= t;
         }
     } // exchange
-    for (i = 0; i < N_dim; i++) {
+    for (short i = 0; i < N_dim; i++) {
         idx_cell.idx3[i] = X[i];
     }
     idx_cell.L = L;
@@ -126,9 +126,9 @@ void makeBaseGrid(Cell (&grid)[N_cell_max]) {
     for (int L=0; L <= L_base; L++) {
         offset = (pow(2, N_dim * L) - 1) / (pow(2, N_dim) - 1);
         dx = 1 / pow(2, L);
-        for (int hindex=0; hindex < pow(2, N_dim * L); hindex++) {
+        for (uint hindex = 0; hindex < pow(2, N_dim * L); hindex++) {
             getHindexInv(hindex, L, idx_cell);
-            for (int i = 0; i < N_dim; i++) {
+            for (short i = 0; i < N_dim; i++) {
                 coord[i] = idx_cell.idx3[i] * dx + dx / 2;
             }
             cell.rho = rhoFunc(coord);
@@ -139,27 +139,97 @@ void makeBaseGrid(Cell (&grid)[N_cell_max]) {
     }
 }
 
-// gaussian
+// Multi-variate Gaussian distribution
 double rhoFunc(const double coord[N_dim], const double sigma = 1.0) {
     double rsq = 0;
-    for (int i = 0; i < N_dim; i++) {
+    for (short i = 0; i < N_dim; i++) {
         rsq += pow(coord[i] - 0.5, 2);
     }
     double rho = exp(-rsq / (2 * sigma)) / pow(2 * M_PI * sigma*sigma, 1.5);
     return rho;
 }
 
+// Criterion for refinement
 bool refCrit(double rho) {
     return rho > rho_crit;
 }
 
-bool checkIfBorder(const idx4 idx_cell, const uint dir, bool (&isBorder)[2]) {
-    isBorder[0] = idx_cell.idx3[dir] == 0;
-    isBorder[1] = idx_cell.idx3[dir] == pow(2, idx_cell.L) - 1;
+// Compute the index of the parent cell
+void getParentIdx(const idx4 idx_cell, idx4 idx_parent) {
+    for (short i = 0; i < N_dim; i++) {
+        idx_parent.idx3[i] = idx_cell.idx3[i] / 2;
+    }
+    idx_parent.L = idx_cell.L - 1;
 }
 
-void grad_cell(idx4 idx_cell, Cell (&grid)[N_cell_max]) {
+// Compute the indices of the neighbor cells in a given direction
+void getNeighborIdx(const idx4 idx_cell, const uint dir, const bool pos, idx4 &idx_neighbor) {
+    for (short i = 0; i < N_dim; i++) {
+        idx_neighbor.idx3[i] = idx_cell.idx3[i] + (int(pos) * 2 - 1) * int(i == dir);
+    }
+    idx_neighbor.L = idx_cell.L;
+}
+
+// Check if a cell exists
+bool checkIfExists(const idx4 idx_cell) {
+    return !(hashtable.find(idx_cell) == hashtable.end());
+}
+
+// Check if a cell face in a give direction is a border of the computational domain
+void checkIfBorder(const idx4 idx_cell, const uint dir, const bool pos, bool &is_border) {
+    is_border = idx_cell.idx3[dir] == int(pos) * (pow(2, idx_cell.L) - 1);
+}
+
+// get information about the neighbor cell necessary for computing the gradient
+void getNeighborInfo(const idx4 idx_cell, const uint dir, const bool pos, bool &is_ref, double &rho) {
+    idx4 idx_neighbor;
+    uint idx1_parent_neighbor;
+    bool is_border, is_notref;
+    // check if the cell is a border cell
+    checkIfBorder(idx_cell, dir, pos, is_border);
+    // compute the index of the neighbor on the same level
+    getNeighborIdx(idx_cell, dir, pos, idx_neighbor);
+    // if the neighbor on the same level does not exist and the cell is not a border cell, then the neighbor is not refined
+    is_notref = checkIfExists(idx_neighbor) && !is_border;
+    is_ref = !is_notref && !is_border;
+    // if the cell is a border cell, set the neighbor index to the cell index (we just want a valid key for the hashtable)
+    // if the neighbor is not refined, set the neighbor index to the index of the parent cell's neighbor
+    // if the neighbor is refined, don't change the neighbor index
+    for (short i = 0; i < N_dim; i++) {
+        idx1_parent_neighbor = idx_cell.idx3[i] / 2 + (int(pos) * 2 - 1) * int(i == dir);
+        idx_neighbor.idx3[i] = idx_cell.idx3[i] * int(is_border) + idx_neighbor.idx3[i] * int(is_ref) + idx1_parent_neighbor * int(is_notref);
+    }
+    // subtract one from the AMR level if the neighbor is not refined
+    idx_neighbor.L = idx_cell.L - int(is_notref);
+    // if the cell is a border cell, use the boundary condition
+    rho = hashtable[idx_neighbor].rho * int(!is_border) + rho_boundary * int(is_border);
+}
+
+// compute the gradient for one cell
+void calcGradCell(idx4 idx_cell) {
+    bool is_ref[2];
+    double dx, rho[3];
+    int fd_case;
     Cell cell = hashtable[idx_cell];
+    dx = pow(0.5, idx_cell.L);
+    rho[2] = cell.rho;
+    for (short dir = 0; dir < N_dim; dir++) {
+        for (short pos = 0; pos < 2; pos++) {
+            getNeighborInfo(idx_cell, dir, pos, is_ref[pos], rho[pos]);
+        }
+        fd_case = is_ref[0] + 2 * is_ref[1];
+        cell.rho_grad[dir] = (fd_kernel[fd_case][0] * rho[0] + fd_kernel[fd_case][1] * rho[2] + fd_kernel[fd_case][2] * rho[1]) / (fd_kernel[fd_case][3] * dx);
+    }
+    hashtable[idx_cell] = cell;
+}
+
+// compute the gradient
+void calcGrad() {
+    idx4 idx_cell;
+    for (hashtable_itr = hashtable.begin(); hashtable_itr != hashtable.end(); hashtable_itr++) {
+        idx_cell = hashtable_itr->first;
+        calcGradCell(idx_cell);
+    }
 }
 
 int main() {
--- amr-cpu.h
+++ amr-cpu.h
@@ -4,14 +4,32 @@ const int L_max = 6;
 const int N_dim = 3;
 const int N_cell_max = 10000;
 const double rho_crit = 0.1;
+const double rho_boundary = 0.; // boundary condition
+
+/*
+Finite difference kernel
+df/dx = [ col0 * f(x_0) + col1 * f(x_1) + col2 * f(x_2) ] / col3
+row0: x_0 = -1.5, x_1 = 0, x_2 = 1.5
+row1: x_0 = -1, x_1 = 0, x_2 = 1.5
+row2: x_0 = -1.5, x_1 = 0, x_2 = 1
+row3: x_0 = -1, x_1 = 0, x_2 = 1
+*/
+const int fd_kernel[4][4] = {
+    {-1, 0, 1, 3},
+    {-9, 5, 4, 15},
+    {-4, -5, 9, 15},
+    {-1, 0, 1, 2}
+};
 
 struct idx4 {
     uint idx3[N_dim];
     uint L;
 };
+
 struct Cell {
     double rho;
     bool flag_leaf;
+    double rho_grad[3];
 };
 
 unsigned int transposeToHilbert(const unsigned int X[N_dim], const int L);
