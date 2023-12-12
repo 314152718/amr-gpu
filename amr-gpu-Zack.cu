51af0fa0dcccc83c506547eda1926a593aa70704
Roma Bhattacharjee <roma0615@gmail.com>
2023-12-12 01:21:36 -0500

    applied stash; some compile errors

--- amr-gpu-Zack.cu
+++ amr-gpu-Zack.cu
@@ -29,9 +29,18 @@ const int32_t LMAX = 6; // max AMR level
 const int32_t NDIM = 3; // number of dimensions
 const int32_t NMAX = 2097152 + 10; // maximum number of cells
 const __device__ int32_t HASH[4] = {-1640531527, 97, 1003313, 5}; // hash function constants
+// TODO change to __device__ when we run calcGrad on GPU
+const __host__ double FD_KERNEL[4][4] = {
+    {-1., 0., 1., 3.},
+    {-9., 5., 4., 15.},
+    {-4., -5., 9., 15.},
+    {-1., 0., 1., 2.}
+};
 const double rho_crit = 0.01; // critical density for refinement
+const double rho_boundary = 0.; // boundary condition
 const double sigma = 0.001; // std of Gaussian density field
 const double EPS = 0.000001;
+const string outfile_name = "grid-gpu.csv";
 
 // custom key type
 struct idx4 {
@@ -62,23 +71,23 @@ struct idx4_equals {
 
 // custom value type
 struct Cell {
-    int32_t rho, rho_grad_x, rho_grad_y, rho_grad_z;
+    int32_t rho;
+    double rho_grad[3];
     int8_t flag_leaf;
 
     __host__ __device__ Cell() {}
-    __host__ __device__ Cell(int32_t rho_init, int32_t rho_grad_x_init, int32_t rho_grad_y_init, int32_t rho_grad_z_init, 
-        int8_t flag_leaf_init) : rho{rho_init}, rho_grad_x{rho_grad_x_init}, rho_grad_y{rho_grad_y_init}, 
-        rho_grad_z{rho_grad_z_init}, flag_leaf{flag_leaf_init} {}
+    __host__ __device__ Cell(int32_t rho_init, double rho_grad_x_init, double rho_grad_y_init, double rho_grad_z_init, 
+        int8_t flag_leaf_init) : rho{rho_init}, rho_grad{rho_grad_x_init, rho_grad_y_init, rho_grad_z_init}, flag_leaf{flag_leaf_init} {}
 
     __host__ __device__ bool operator==(Cell const& other) const {
-        return abs(rho - other.rho) < EPS && abs(rho_grad_x - other.rho_grad_x) < EPS
-            && abs(rho_grad_y - other.rho_grad_y) < EPS && abs(rho_grad_z - other.rho_grad_z) < EPS;
+        return abs(rho - other.rho) < EPS && abs(rho_grad[0] - other.rho_grad[0]) < EPS
+            && abs(rho_grad[1] - other.rho_grad[1]) < EPS && abs(rho_grad[2] - other.rho_grad[2]) < EPS;
     }
 };
 
 ostream& operator<<(ostream &os, Cell const &cell) {
-    os << "[rho " << cell.rho << ", rho_grad_x " << cell.rho_grad_x << ", rho_grad_y"
-       << cell.rho_grad_y << ", rho_grad_z " << cell.rho_grad_z << ", flag_leaf " << cell.flag_leaf << "]";
+    os << "[rho " << cell.rho << ", rho_grad_x " << cell.rho_grad[0] << ", rho_grad_y"
+       << cell.rho_grad[1] << ", rho_grad_z " << cell.rho_grad[2] << ", flag_leaf " << cell.flag_leaf << "]";
     return os;
 }
 
@@ -105,11 +114,13 @@ void makeBaseGrid(Cell (&grid)[NMAX], map_type &hashtable);
 void setGridCell(const idx4 idx_cell, const int hindex, int8_t flag_leaf, map_type &hashtable);
 Cell* find(map_type &hashtable, const idx4& key);
 void insert(map_type &hashtable, const idx4& key, Cell* const value);
+void getNeighborInfo(const idx4 idx_cell, const int dir, const bool pos, bool &is_ref, double &rho_neighbor, map_type &hashtable);
+void calcGradCell(const idx4 idx_cell, Cell* cell, map_type &hashtable);
+void calcGrad(map_type &hashtable);
+void writeGrid(map_type &hashtable);
+thrust::zip_iterator<thrust::tuple<idx4, Cell*>> retrieve_zipped(map_type &hashtable);
 
 // ------- GLOBALS --------- //
-
-thrust::device_vector<idx4> insert_keys;
-thrust::device_vector<Cell*> insert_values;
 
 
 // ------------------------------------------------ //
@@ -295,16 +306,11 @@ void setGridCell(const idx4 idx_cell, const int hindex, int8_t flag_leaf, map_ty
 */
 // TODO: this could probably also run on GPU (using a device view)
 void insert(map_type &hashtable, const idx4& key, Cell* const value) {
-    if (insert_keys.size() < 1) {
-        insert_keys.push_back(key);
-    } else {
-        insert_keys[0] = key;
-    }
-    if (insert_values.size() < 1) {
-        insert_values.push_back(value);
-    } else {
-        insert_values[0] = value;
-    }
+    thrust::device_vector<idx4> insert_keys;
+    thrust::device_vector<Cell*> insert_values;
+    insert_keys.push_back(key);
+    insert_values.push_back(value);
+    // todo change this to just inserting using a pair, zip is unnecessary
     auto zipped =
         thrust::make_zip_iterator(thrust::make_tuple(insert_keys.begin(), insert_values.begin()));
 
@@ -329,8 +335,7 @@ void refineGridCell(const idx4 idx_cell, map_type &hashtable) {
     int hindex;
     getHindex(idx_cell, hindex);
 
-    Cell *pCell;
-    pCell = find(idx_cell, hashtable);
+    Cell *pCell = find(hashtable, idx_cell);
     if (pCell == empty_pcell_sentinel) throw runtime_error("Trying to refine non-existant cell!");
 
     if (!pCell->flag_leaf) throw runtime_error("trying to refine non-leaf");
@@ -366,22 +371,27 @@ void refineGridCell(const idx4 idx_cell, map_type &hashtable) {
     }
 }
 
-void refineGrid1lvl(map_type& hashtable) {
-    // rewrite with retrieve_all
+thrust::zip_iterator<thrust::tuple<idx4, Cell*>> retrieve_zipped(map_type& hashtable) {
     size_t numCells = hashtable.get_size();
     thrust::device_vector<idx4> retrieved_keys(numCells);
     thrust::device_vector<Cell*> retrieved_values(numCells);
-    hashtable.retrieve_all(retrieved_keys.begin(), retrieved_values.begin()); // doesn't populate values for some reason
+    hashtable.retrieve_all(retrieved_keys.begin(), retrieved_values.begin());               // doesn't populate values for some reason
     hashtable.find(retrieved_keys.begin(), retrieved_keys.end(), retrieved_values.begin()); // this will populate values
 
-    auto zipped = thrust::make_zip_iterator(thrust::make_tuple(retrieved_keys.begin(), retrieved_values.begin()));
+    auto zipped = 
+        thrust::make_zip_iterator(thrust::make_tuple(retrieved_keys.begin(), retrieved_values.begin()));
+    return zipped;
+}
+
+void refineGrid1lvl(map_type& hashtable) {
+    auto zipped = retrieve_zipped(hashtable);
     idx4 idx_cell;
     Cell* pCell;
-    for (auto it = zipped; it != zipped + retrieved_keys.size(); it++) {
+    for (auto it = zipped; it != zipped + hashtable.get_size(); it++) {
         thrust::tuple<idx4, Cell*> t = *it;
         idx_cell = t.get<0>();
         pCell = t.get<1>();
-        cout << "Retrieved pair: " << idx_cell << ", " << pCell << endl;
+        std::cout << "Retrieved pair: " << idx_cell << ", " << pCell << endl;
         if (refCrit(pCell->rho) && pCell->flag_leaf) {
             refineGridCell(idx_cell, hashtable);
         }
@@ -392,7 +402,86 @@ void refineGrid1lvl(map_type& hashtable) {
 // all tests (later move out)
 
 void test_hashtable_set_leafflag() {
-    cout << "HELLO" << endl;
+    std::cout << "HELLO" << endl;
+}
+
+// get information about the neighbor cell necessary for computing the gradient
+void getNeighborInfo(const idx4 idx_cell, const int dir, const bool pos, bool &is_ref, double &rho_neighbor, map_type &hashtable) {
+    idx4 idx_neighbor;
+    int idx1_parent_neighbor;
+    bool is_border, is_notref;
+    // check if the cell is a border cell
+    checkIfBorder(idx_cell, dir, pos, is_border);
+    // compute the index of the neighbor on the same level
+    getNeighborIdx(idx_cell, dir, pos, idx_neighbor);
+    // if the neighbor on the same level does not exist and the cell is not a border cell, then the neighbor is not refined
+    is_notref = !checkIfExists(idx_neighbor, hashtable) && !is_border;
+    is_ref = !is_notref && !is_border;
+    // if the cell is a border cell, set the neighbor index to the cell index (we just want a valid key for the hashtable)
+    // if the neighbor is not refined, set the neighbor index to the index of the parent cell's neighbor
+    // if the neighbor is refined, don't change the neighbor index
+    for (short i = 0; i < NDIM; i++) {
+        idx1_parent_neighbor = idx_cell.idx3[i] / 2 + (int(pos) * 2 - 1) * int(i == dir);
+        idx_neighbor.idx3[i] = idx_cell.idx3[i] * int(is_border) + idx_neighbor.idx3[i] * int(is_ref) + idx1_parent_neighbor * int(is_notref);
+    }
+    // subtract one from the AMR level if the neighbor is not refined
+    idx_neighbor.L = idx_cell.L - int(is_notref);
+    // if the cell is a border cell, use the boundary condition
+    Cell* pCell = find(hashtable, idx_neighbor);
+    rho_neighbor = pCell->rho * int(!is_border) + rho_boundary * int(is_border);
+}
+
+// compute the gradient for one cell
+void calcGradCell(const idx4 idx_cell, Cell* cell, map_type &hashtable) {
+    bool is_ref[2];
+    double dx, rho[3];
+    int fd_case;
+    dx = pow(0.5, idx_cell.L);
+    rho[2] = cell->rho;
+    for (short dir = 0; dir < NDIM; dir++) {
+        for (short pos = 0; pos < 2; pos++) {
+            getNeighborInfo(idx_cell, dir, pos, is_ref[pos], rho[pos], hashtable);
+        }
+        fd_case = is_ref[0] + 2 * is_ref[1];
+        cell->rho_grad[dir] = (FD_KERNEL[fd_case][0] * rho[0] + FD_KERNEL[fd_case][1] * rho[2] + FD_KERNEL[fd_case][2] * rho[1]) / (FD_KERNEL[fd_case][3] * dx);
+    }
+}
+
+// compute the gradient
+void calcGrad(map_type &hashtable) {
+    thrust::zip_iterator<thrust::tuple<idx4, Cell*>> zipped = retrieve_zipped(hashtable);
+    idx4 idx_cell;
+    Cell* pCell;
+    for (auto it = zipped; it != zipped + hashtable.get_size(); it++) {
+        thrust::tuple<idx4, Cell*> t = *it;
+        idx_cell = t.get<0>();
+        pCell = t.get<1>();
+        calcGradCell(idx_cell, pCell, hashtable);
+    }
+}
+
+void writeGrid(map_type& hashtable) {
+    // save i, j, k, L, rho, gradients for all cells (use the iterator) to a file
+    ofstream outfile;
+    outfile.open(outfile_name);
+    idx4 idx_cell;
+    Cell* pCell;
+    outfile << "i,j,k,L,flag_leaf,rho,rho_grad_x,rho_grad_y,rho_grad_z\n";
+    thrust::zip_iterator<thrust::tuple<idx4, Cell*>> zipped = retrieve_zipped(hashtable);
+    for (auto it = zipped; it != zipped + hashtable.get_size(); it++) {
+        thrust::tuple<idx4, Cell*> t = *it;
+        idx_cell = t.get<0>();
+        pCell = t.get<1>();
+        outfile << idx_cell.idx3[0] << "," << idx_cell.idx3[1] << "," << idx_cell.idx3[2]
+                << "," << idx_cell.L << "," << pCell->flag_leaf << "," << pCell->rho << "," << pCell->rho_grad[0]
+                << "," << pCell->rho_grad[1] << "," << pCell->rho_grad[2] << "\n";
+    }
+    outfile.close();
+}
+
+
+int main() {
+    // Cell empty_cell_sentinel{-1, -1, -1, -1, -1};
     cuco::static_map<idx4, Cell*> hashtable{
         NMAX, cuco::empty_key{empty_idx4_sentinel}, cuco::empty_value{empty_pcell_sentinel}
     };
@@ -403,40 +492,18 @@ void test_hashtable_set_leafflag() {
        refineGrid1lvl(hashtable);
     }
 
-    // auto start = high_resolution_clock::now();
-    // calcGrad();
-    // auto stop = high_resolution_clock::now();
-    // auto duration = duration_cast<milliseconds>(stop - start);
-    // cout << duration.count() << " ms" << endl;
-    // writeGrid();
-
-    thrust::device_vector<idx4> result_keys(73);
-    thrust::device_vector<Cell*> result_values(73);
-    hashtable.find(insert_keys.begin(), insert_keys.end(), result_values.begin());
-
-    cout << "KEYS:" << endl;
-    for (auto k : result_keys) {
-        cout << k << endl;
-    }
-
-    cout << "VALUES:" << endl;
-    for (auto v : result_values) {
-        cout << *v << endl;
-    }
-    cout << endl << endl;
-
-    idx4 idx_cell{0, 0, 0, 1};
-    cout << find(hashtable, idx_cell)->flag_leaf << endl;
-    find(hashtable, idx_cell)->flag_leaf = 1;
-    cout << find(hashtable, idx_cell)->flag_leaf << endl;
+    auto start = high_resolution_clock::now();
+    // TODO: run this on GPU with <<NUM_BLOCKS, NUM_THREADS_PER_BLOCK>> syntax
+    calcGrad(hashtable);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << duration.count() << " ms" << endl;
+    writeGrid(hashtable);
 }
 
-int main() {
+int main3() {
     test_hashtable_set_leafflag();
 }
-
-
-
 
 void test_map_insert_int() {
     cuco::static_map<int32_t, int32_t> hashtable{NMAX, cuco::empty_key{-1}, cuco::empty_value{-1}};
@@ -468,7 +535,7 @@ void test_map_insert_cell_pointer() {
     // Retrieve contents of all the non-empty slots in the map
     thrust::device_vector<idx4> result_keys(2);
     thrust::device_vector<Cell*> result_values(2);
-    hashtable.find(insert_keys.begin(), insert_keys.end(), result_values.begin());
+    // hashtable.find(insert_keys.begin(), insert_keys.end(), result_values.begin());
     // hashtable.retrieve_all(result_keys.begin(), result_values.begin());
 
     // cout << "KEYS:" << endl;
@@ -486,12 +553,8 @@ void test_map_insert_cell_pointer() {
 
     //delete pTest_cell;
 }
-
+ 
 void test_map_insert_cell_pointer_Roma() {
-    cuco::static_map<idx4, Cell*> hashtable{
-        NMAX, cuco::empty_key{empty_idx4_sentinel}, cuco::empty_value{empty_pcell_sentinel}
-    };
-
     idx4 idx_cell{1, 1, 1, 1};
     Cell* pTest_cell = new Cell{1, 1, 1, 1, 1}; // create on heap
     cuco::static_map<idx4, Cell*> hashtable{
@@ -555,7 +618,7 @@ void test_map_insert_cell_pointer_Roma2() {
 
     // check if its in the table correctly?
     Cell* pResult;
-    pResult = find(idx_cell, hashtable);
+    pResult = find(hashtable, idx_cell);
     cout << "Found" << pResult << endl;
     cout << "Size: " << hashtable.get_size() << endl;
 
