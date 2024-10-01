b8113f3f636fe1a9721a7a4ae8d44a9f2a01a2db
Nataliia Khotiaintseva <nk9431@princeton.edu>
2024-10-01 17:42:22 -0400

    all files

---
+++ run_matrix.sh
@@ -0,0 +1,14 @@
+sbatch job_stellar_matrix.slurm
+
+function repeat() {      
+    local times="$1";      
+    local sec="$2";      
+    shift;      
+    shift;      
+    local cmd="$@";      
+    for ((i = 1; i <= $times; i++ )); do         
+        eval "$cmd";        
+        sleep $sec;     
+    done ;  
+}
+repeat 100 5 squeue -u $USER
